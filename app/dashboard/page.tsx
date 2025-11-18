bcfc3a2762e038dc6562a6939d5e98666e60f66c
Osvaldo Deschamps Neto <osvaldobnu@gmail.com>
2025-11-18 08:47:42 -0300

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -54,7 +54,7 @@ export default async function DashboardPage() {
   const bookingsRaw = await db.booking.findMany({
     where: {
       services: { some: { service: { barbershopId } } },
-      // date: { gte: startOfMonth, lte: endOfToday },
+      date: { gte: startOfMonth, lte: endOfToday },
     },
     include: {
       user: true,
@@ -97,10 +97,10 @@ export default async function DashboardPage() {
           : null,
         barbershop: firstService
           ? {
-            id: firstService.barbershop.id,
-            name: firstService.barbershop.name,
-            imageUrl: firstService.barbershop.imageUrl,
-          }
+              id: firstService.barbershop.id,
+              name: firstService.barbershop.name,
+              imageUrl: firstService.barbershop.imageUrl,
+            }
           : { id: "", name: "Sem barbearia", imageUrl: "" },
         services: b.services.map((s) => ({
           name: s.service.name,
@@ -124,17 +124,15 @@ export default async function DashboardPage() {
             <p className="text-3xl font-bold text-gray-700">{uniqueClients}</p>
           </div>
 
-          {/* Comentado para uso futuro
-           <div className="bg-white shadow-md rounded-lg p-6 flex flex-col items-center">
+          <div className="bg-white shadow-md rounded-lg p-6 flex flex-col items-center">
             <FaMoneyBillWave className="text-3xl text-green-600 mb-2" />
             <h3 className="text-lg font-semibold text-gray-800">Receita do Mês</h3>
             <p className="text-3xl font-bold text-gray-700">
               R$ {totalRevenueThisMonth.toFixed(2)}
             </p>
           </div>
-        
-        */}
         </div>
+
         <BarberCalendar bookings={sanitizedBookings} />
       </main>
     </div>
